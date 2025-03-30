803629e43e4e219ca3bd757ee33965d8ad41816e
Bruno <idev4tune@gmail.com>
2025-03-29 23:31:55 -0300

    project created

---
+++ src/main/java/io/github/idev4tune/contact_book_api/ContactBookApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.idev4tune.contact_book_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactBookApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactBookApiApplication.class, args);
+	}
+
+}
