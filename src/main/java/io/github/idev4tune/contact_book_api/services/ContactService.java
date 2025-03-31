d65e1ea21d4e629d3368c1bd8c891fbdc3ab08f1
Bruno <idev4tune@gmail.com>
2025-03-31 20:10:57 -0300

    service layer

---
+++ src/main/java/io/github/idev4tune/contact_book_api/services/ContactService.java
@@ -0,0 +1,7 @@
+package io.github.idev4tune.contact_book_api.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public interface ContactService {
+}
---
+++ src/main/java/io/github/idev4tune/contact_book_api/services/implementation/ContactServiceImpl.java
@@ -0,0 +1,4 @@
+package io.github.idev4tune.contact_book_api.services.implementation;
+
+public class ContactServiceImpl {
+}
