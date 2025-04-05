f4b9fad12a3643ded47b3f368edfa5b91f0b0efc
Bruno <idev4tune@gmail.com>
2025-04-04 23:06:33 -0300

    Recording a Contact via CommandLineRunner Interface

--- src/main/java/io/github/idev4tune/contact_book_api/ContactBookApiApplication.java
+++ src/main/java/io/github/idev4tune/contact_book_api/ContactBookApiApplication.java
@@ -1,10 +1,26 @@
 package io.github.idev4tune.contact_book_api;
 
+import io.github.idev4tune.contact_book_api.entities.Contact;
+import io.github.idev4tune.contact_book_api.repositories.ContactRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ContactBookApiApplication {
+
+	@Bean
+	public CommandLineRunner saveContact(@Autowired ContactRepository repository) {
+		return args -> {
+			Contact contact = new Contact();
+			contact.setName("Bruno");
+			contact.setEmail("bruno@commandlinerunner.com");
+			contact.setFavorite(true);
+			repository.save(contact);
+		};
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ContactBookApiApplication.class, args);
